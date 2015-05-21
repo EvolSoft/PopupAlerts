dea17924ec92e8e6b264ff187a0e857c3cab7c4c
Flavius12 <info@evolsoft.tk>
2015-05-21 14:56:26 +0200

    Update MessageTask.php

--- src/PopupAlerts/MessageTask.php
+++ src/PopupAlerts/MessageTask.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * PopupAlerts (v1.1) by EvolSoft
+ * PopupAlerts (v1.2) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
- * Date: 17/04/2015 09:55 AM (UTC)
+ * Date: 21/05/2015 01:16 PM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/PopupAlerts/blob/master/LICENSE)
  */
